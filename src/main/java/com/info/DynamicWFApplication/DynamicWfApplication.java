dda31c1e4c26ada5b296a9cfc265f44de8f65681
sundargaint <91435185+sundargaint@users.noreply.github.com>
2022-01-09 16:04:43 +0530

    updated

---
+++ src/main/java/com/info/DynamicWFApplication/DynamicWfApplication.java
@@ -0,0 +1,13 @@
+package com.info.DynamicWFApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicWfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicWfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/info/DynamicWFApplication/DynamicWfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.DynamicWFApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicWfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
